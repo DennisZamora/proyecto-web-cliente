fea4ed9708e5789742a029a17513fc3719249321
shu353 <shu-3096@hotmail.com>
2021-08-01 21:05:00 -0600

    noticias en cada una de las categorias

--- MySQL/scriptDB.sql
+++ MySQL/scriptDB.sql
@@ -54,8 +54,41 @@ insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
 ('Lorem','No his munere interesset. At soluta accusam gloriatur eos, ferri commodo sed id,
  ei tollit legere nec. Eum et iudico graecis, cu zzril instructior per, usu at augue epicurei.
  Saepe scaevola takimata vix id. Errem dictas posidonium id vis, ne modo affert incorrupte eos.',1,1);
- 
- select * from blog;
+
+insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('Improved Command Line Integration','Integrating the shell functionality in DevOps operations is a key feature and this release has introduced a big improvement on this area being the most remarkable improvements the following:
+
+No longer need to execute APIs using the –execute (-e) command line argument: all of the data required for any API available in CLI can be defined through command line arguments (including lists).',1,1);
+
+insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('SkySQL, MariaDB-as-a-service, launches on Google Cloud','Today MariaDB Corporation officially launched SkySQL, a cloud-hosted version of the MariaDB database management system supported and managed by MariaDB’s own creators.
+
+Aside from providing convenient access to MariaDB on Google Cloud Platform starting at $0.45 per hour, SkySQL strikes back at services like Google Cloud SQL and Amazon RDS — managed database solutions that feature forks of MySQL or MariaDB, but are often multiple versions behind the latest releases. Amazon RDS, for instance, only supports up to MariaDB 10.3, whereas MariaDB 10.4.12 is the most recent version.',1,1);
+
+insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('SolidJS creator: JavaScript innovation isn’t slowing down','One of the most interesting JavaScript frameworks gaining traction these days is SolidJS. Solid is interesting because it takes JSX (React’s templating language) in novel directions. It decorates JSX with a handful of reactive primitives. It is compiled (similar to Svelte). It layers on higher-order services (like a central store and events). And for good measure, it tosses in full-featured SSR (server-side rendering) and Suspense.
+
+I talked to Solid’s creator, Ryan Carniato, about how he and the Solid team pulled this off, how Solid relates to industry developments, how innovative front-end features (like partial hydration, streaming SSR, and Suspense) are implemented, and what motivates him to keep pushing the limits of front-end JavaScript performance.',1,2);
+
+insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('TypeScript 4.4 brings performance boosts
+','Microsoft has released a beta version of TypeScript 4.4, the latest planned version of its popular typed version of JavaScript, with capabilities including performance improvements and control flow analysis.
+
+For faster declaration emits, TypeScript now caches whether internal symbols are accessible in different contexts, along with how specific types are to be printed. This improves general performance in code with fairly complex types. Other performance enhancements in TypeScript 4.4 promise faster path normalization and path mapping, along with faster incremental builds. Also, an optimization has been added for source map generation of very large output files.',1,2);
+
+insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('The case for HTML5 over mobile apps','There is no better time than now to test mobile marketing.
+
+Mobile marketing is delivering great results and valuable data for many categories of marketers. Part of remaining relevant is having a mobile marketing strategy that allows users to engage with marketing content across multiple platforms.
+
+Mobile can extend the conversation from traditional channels, help to activate prospects and customers, and provide mechanisms for tracking effectiveness across channels.',1,3);
+
+insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('Lorem','No his munere interesset. At soluta accusam gloriatur eos, ferri commodo sed id,
+ ei tollit legere nec. Eum et iudico graecis, cu zzril instructior per, usu at augue epicurei.
+ Saepe scaevola takimata vix id. Errem dictas posidonium id vis, ne modo affert incorrupte eos.',1,1);
+
+select * from blog;
  
  select b.nombre,a.tituloBlog,a.contenidoBlog,a.fecha_publicacion,c.nombreCategoria  from blog a,usuario b,categoria c
  where a.idUsuario = b.idUsuario and
