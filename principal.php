000b634166daf3168eeb3a5a075ab07e54d76e4a
shu353 <shu-3096@hotmail.com>
2021-08-15 23:00:35 -0600

    file getCategoria creado

--- principal.php
+++ principal.php
@@ -74,7 +74,7 @@
         <div class="container px-4 px-lg-5">
         
             <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="col-md-10 col-lg-8 col-xl-7" id="blogs">
                     
                     <?php foreach($resultado as $blog){ ?>
 
