f06b58c925474b7f7f58c6d04690f07e643ffe0f
DennisZamora <78609499+DennisZamora@users.noreply.github.com>
2021-08-18 16:37:53 -0600

    Se elimino comnado innecesarios,se agrego dos archivos nuevos para mostrar los blogs

---
+++ consulta.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'conexiones/conexion.php';
+
+function consulta($consulta)
+{
+    $conexion = Conecta();
+    if (!$conexion->set_charset("utf8")) {
+        printf("Error de los caracteres utf8: %s\n", $conexion->error);
+    } 
+    $respuesta = $conexion->query($consulta);
+    return $respuesta;
+}
+?>
