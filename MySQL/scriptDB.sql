1739eff0d46de832fee89e05cc7d088ff5dade8e
DennisZamora <78609499+DennisZamora@users.noreply.github.com>
2021-08-19 17:46:49 -0600

    Cambios en la base de datos

--- MySQL/scriptDB.sql
+++ MySQL/scriptDB.sql
@@ -47,52 +47,53 @@ insert into usuario (nombre,last_name,username,email,contrasena,idRol)values
 insert into usuario (nombre,last_name,username,email,contrasena,idRol)values
 ('Jesus','Vargas','jesus','jesusvargas@hotmail.com','jesus123',2);
 
-  
 insert into categoria (nombreCategoria) values 
 ('MySQL'),
 ('JavaScript'),
-('HTML5');
+('HTML5'),
+('PHP');
 
 insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('Lorem','No his munere interesset. At soluta accusam gloriatur eos, ferri commodo sed id,
- ei tollit legere nec. Eum et iudico graecis, cu zzril instructior per, usu at augue epicurei.
- Saepe scaevola takimata vix id. Errem dictas posidonium id vis, ne modo affert incorrupte eos.',1,1);
-
-insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('Improved Command Line Integration','Integrating the shell functionality in DevOps operations is a key feature and this release has introduced a big improvement on this area being the most remarkable improvements the following:
-
-No longer need to execute APIs using the –execute (-e) command line argument: all of the data required for any API available in CLI can be defined through command line arguments (including lists).',1,1);
-
-insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('SkySQL, MariaDB-as-a-service, launches on Google Cloud','Today MariaDB Corporation officially launched SkySQL, a cloud-hosted version of the MariaDB database management system supported and managed by MariaDB’s own creators.
-
-Aside from providing convenient access to MariaDB on Google Cloud Platform starting at $0.45 per hour, SkySQL strikes back at services like Google Cloud SQL and Amazon RDS — managed database solutions that feature forks of MySQL or MariaDB, but are often multiple versions behind the latest releases. Amazon RDS, for instance, only supports up to MariaDB 10.3, whereas MariaDB 10.4.12 is the most recent version.',1,1);
-
-insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('SolidJS creator: JavaScript innovation isn’t slowing down','One of the most interesting JavaScript frameworks gaining traction these days is SolidJS. Solid is interesting because it takes JSX (React’s templating language) in novel directions. It decorates JSX with a handful of reactive primitives. It is compiled (similar to Svelte). It layers on higher-order services (like a central store and events). And for good measure, it tosses in full-featured SSR (server-side rendering) and Suspense.
-
-I talked to Solid’s creator, Ryan Carniato, about how he and the Solid team pulled this off, how Solid relates to industry developments, how innovative front-end features (like partial hydration, streaming SSR, and Suspense) are implemented, and what motivates him to keep pushing the limits of front-end JavaScript performance.',1,2);
-
-insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('TypeScript 4.4 brings performance boosts
-','Microsoft has released a beta version of TypeScript 4.4, the latest planned version of its popular typed version of JavaScript, with capabilities including performance improvements and control flow analysis.
-
-For faster declaration emits, TypeScript now caches whether internal symbols are accessible in different contexts, along with how specific types are to be printed. This improves general performance in code with fairly complex types. Other performance enhancements in TypeScript 4.4 promise faster path normalization and path mapping, along with faster incremental builds. Also, an optimization has been added for source map generation of very large output files.',1,2);
-
-insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('The case for HTML5 over mobile apps','There is no better time than now to test mobile marketing.
-
-Mobile marketing is delivering great results and valuable data for many categories of marketers. Part of remaining relevant is having a mobile marketing strategy that allows users to engage with marketing content across multiple platforms.
-
-Mobile can extend the conversation from traditional channels, help to activate prospects and customers, and provide mechanisms for tracking effectiveness across channels.',1,3);
-
-insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
-('Lorem','No his munere interesset. At soluta accusam gloriatur eos, ferri commodo sed id,
- ei tollit legere nec. Eum et iudico graecis, cu zzril instructior per, usu at augue epicurei.
- Saepe scaevola takimata vix id. Errem dictas posidonium id vis, ne modo affert incorrupte eos.',1,1);
-
-select * from blog;
+('OPINION ABOUT MYSQL','MySQL is one of the best databases, very light,
+ very easy to install and I disagree with the previous 
+ comment about help, MySQL has hundreds and hundreds of
+ help pages, you just have to enter the Mysql website. 
+ The Payment Bases, it already has Stored Procedures, Triggers, Views, etc.',1,1);
  
- select b.nombre,a.tituloBlog,a.contenidoBlog,a.fecha_publicacion,c.nombreCategoria  from blog a,usuario b,categoria c
- where a.idUsuario = b.idUsuario and
- c.idCategoria = a.idCategoria;
+ insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('MYSQL NEEDS','MySQL was able to meet all my 
+expectations in distinctive areas. Manage multiple tables
+ for different services. Recover in disaster. 
+ An agile and secure deployment of templates
+ captured on the command line.',2,1);
+ 
+ insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('OPINION ABOUT HTML5 ','Excellent changes, the problem is who still uses IE6 
+and that there are several and there is still time for it to be completely eradicated. 
+The other browsers such as Opera and FF are sure to apply it and be guided according to 
+the standards, on the other hand IE7 will surely come out with something proprietary. 
+I hope not...',1,3);
+ 
+ insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('HTML5 ','I think that it will be very cool since it 
+will simplify some things such as the new video tag and the footer, 
+also remembering that not only can you use one h1 or h2 if not several
+ in the same document but there is still a lot to be done for full
+ compatibility but the best browser personally compatible is explorer
+ and safari.',2,3);
+ 
+  insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('JAVASCRIPT DEFINITION','JavaScript is one of the most widely 
+used and well-known programming languages, since it allows you 
+to create dynamic and attractive pages in which you can interact 
+more with users; for example, thanks to this language the most 
+attractive visual experience of the user when updating your
+ Facebook, Instagram, Twitter and other social networks status',1,2);
+ 
+ insert into blog (tituloBlog,contenidoBlog,idUsuario,idCategoria) values
+('JAVA ADVANTAGES','Javascript is a very simple language, 
+it is fast, therefore it tends to execute the functions 
+immediately, it also has multiple options for visual effects.
+It is supported by the most popular browsers and is 
+compatible with the most modern devices, including iPhone, mobiles and PS3. ',2,2);
+
